1be855a84caab7a328687de75e1666fc3a74600c
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-29 14:49:51 +0000

    Add line 9 and comment out

--- src/database-connection.js
+++ src/database-connection.js
@@ -6,6 +6,7 @@ const dbName = process.env.MONGODB_DATABASE
 
 // eslint-disable-next-line prefer-const
 let connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017'
+// let connectionString = process.env.`mongodb+srv://${username}:<${password}>@cluster0.mqz6yke.mongodb.net/${dbName}?retryWrites=true&w=majority` || 'mongodb://localhost:27017'
 
 mongoose.set('debug', true)
 
@@ -16,3 +17,7 @@ mongoose
   })
   .then(() => console.log('connection established'))
   .catch(error => console.log('not connected:', error))
+
+const Panda = mongoose.model('Panda', { name: String })
+const lili = new Panda({ name: 'lili' })
+lili.save().then(() => console.log('we have a new panda!'))
