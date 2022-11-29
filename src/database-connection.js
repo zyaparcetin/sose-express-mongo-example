f75ba264cf7d52b4b4ba6afc747a5cd0f506c353
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-29 15:55:20 +0000

    Delete username,password and dbName variables

--- src/database-connection.js
+++ src/database-connection.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose')
 
-const username = process.env.MONGODB_USERNAME
-const password = process.env.MONGODB_PASSWORD
-const dbName = process.env.MONGODB_DATABASE
-
 // eslint-disable-next-line prefer-const
 let connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017'
-// let connectionString = process.env.`mongodb+srv://${username}:<${password}>@cluster0.mqz6yke.mongodb.net/${dbName}?retryWrites=true&w=majority` || 'mongodb://localhost:27017'
 
 mongoose.set('debug', true)
 
