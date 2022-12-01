25e0a4c2f2e1ad5d4cc417c27fe023a2405cf061
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-01 08:50:28 +0000

    Create user.js

--- src/models/user.js
+++ src/models/user.js
@@ -39,3 +39,39 @@ userSchema.loadClass(User)
 userSchema.plugin(autopopulate)
 
 module.exports = mongoose.model('User', userSchema)
+
+/* 
+const Review = require('./review')
+
+class User {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+    this.likes = []
+    this.basket = []
+    this.reviews = []
+  }
+
+  greet() {
+    console.log(`Hello ${this.name}, welcome to Joy Toys!`)
+  }
+
+  likeProduct(product) {
+    this.likes.push(product)
+    product.likedBy.push(this.name)
+  }
+
+  addToBasket(product) {
+    this.basket.push(product)
+  }
+
+  review(product, text, rate) {
+    const review = new Review(text, rate, this.name)
+    this.reviews.push(review)
+    product.reviews.push(review)
+  }
+}
+
+module.exports = User
+
+*/
