e355ac296903ee81058b57669b2879ae10950330
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-01 09:08:37 +0000

    Add my index.js and comment out

--- src/database-connection.js
+++ src/database-connection.js
@@ -23,3 +23,35 @@ async function main() {
 main() */
 // const panda = new Panda({ name: 'anna', age: 19 })
 // panda.save().then(() => console.log(`we have a new panda, ${panda.name}!`))
+
+/* my index.js
+const User = require('./user')
+const Product = require('./product')
+// const Review = require('./review')
+
+const kerem = new User('kerem', 11)
+const mete = new User('mete', 7)
+
+const teddyBear = new Product('teddy bear', 19.99)
+const kitty = new Product('kitty', 14.99)
+const bunny = new Product('bunny', 9.99)
+const froglet = new Product('froglet', 9.99)
+const products = [teddyBear, kitty, bunny, froglet]
+
+mete.greet()
+
+kerem.addToBasket(kitty)
+mete.addToBasket(teddyBear)
+
+kerem.likeProduct(kitty)
+
+kerem.review(kitty, 'Wonderful toy!', 5)
+
+console.log(kerem)
+console.log(mete)
+
+console.log(kitty)
+
+module.exports = { products }
+
+*/
