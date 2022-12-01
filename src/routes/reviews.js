55c089819152d2291e6c1bc132b4dbd1d84bd700
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-01 10:32:37 +0000

    Add some lines and comment out

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,8 +1,20 @@
 const express = require('express')
-const { reviews } = require('../models')
+// const { reviews } = require('../models') // Terminal: 'reviews' is assigned a value but never used.
 
 const router = express.Router()
 
-/* GET products listing. */
+/* GET reviews listing. */
+/* router.get('/', (req, res, next) => {
+  res.send(products)
+})
 
+router.get('/:productId', (req, res, next) => {
+  const product = products[req.params.productId]
+  if (!product)
+    return res.render('error', {
+      error: { status: 404 },
+      message: `No product with id ${req.params.id} found`,
+    })
+  return res.send(product)
+}) */
 module.exports = router
