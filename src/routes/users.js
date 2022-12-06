3bde6863a8801e7cfe593ae7abb96ec5689a1aba
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-06 21:20:50 +0000

    Delete User, Product and Get Initialize part

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
+
 const User = require('../models/user')
-const Product = require('../models/product')
 
 const router = express.Router()
 
@@ -8,23 +8,6 @@ const router = express.Router()
 router.get('/', async (req, res) => {
   const query = {}
   res.send(await User.find(query).catch(error => console.log('Users not found, error: ', error)))
-})
-
-/* GET initialize */
-router.get('/initialize', async (req, res) => {
-  const Kerem = await User.create({ name: 'Kerem', age: 11 })
-  const Mete = await User.create({ name: 'Mete', age: 7 })
-
-  const teddyBearProduct = await Product.create({ name: 'teddyBear', price: 19.99 })
-  const kittyProduct = await Product.create({ name: 'kitty', price: 14.99 })
-  const bunnyProduct = await Product.create({ name: 'bunny', price: 9.99 })
-  const frogletProduct = await Product.create({ name: 'froglet', price: 9.99 })
-
-  await Kerem.likeProduct(teddyBearProduct)
-  await Kerem.addToBasket(teddyBearProduct)
-
-  console.log(Kerem)
-  res.sendStatus(200)
 })
 
 /* POST user */
